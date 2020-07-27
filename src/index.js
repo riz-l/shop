f00be472bcde29128cb1a99cac9581638f24cb91
Riz <joshua.riz.layton@gmail.com>
2020-07-27 23:35:12 +0100

    Added starting files (create-react-app)

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// Import: Dependencies
+import React from "react";
+import ReactDOM from "react-dom";
 
+// Import: Styling
+import "./index.css";
+
+// Import: Components
+import App from "./App";
+
+// Import: Service Worker
+import * as serviceWorker from "./serviceWorker";
+
+// Render: App
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
+// Register: Service Worker
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
